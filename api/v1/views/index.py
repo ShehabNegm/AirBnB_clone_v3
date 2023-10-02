9435562c8327b69bc6785fcd32f23d02ab30ff9f
ShehabNegm <shebonegm@gmail.com>
2023-10-02 22:55:10 +0200

    add route stats to index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,23 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """status endpoint"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def get_count():
+    count_dict = {"amenities": 'Amenity',
+                  "cities": 'City',
+                  "places": 'Place',
+                  "reviews": 'Review',
+                  "states": 'State',
+                  "users": 'User'}
+
+    for k in count_dict.keys():
+        count_dict[k] = storage.count(count_dict.get(k))
+    return jsonify(count_dict)
