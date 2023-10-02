29fde1fefe8d2dfd1fbac9599ee6199affee028b
Amina Amer <amina.amer.9898@gmail.com>
2023-10-02 13:59:49 -0700

    Handle 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """This module is to create an instance of Flask"""
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,6 +17,16 @@ def teardown(exception):
     """Handles teardown by calling storage.close()"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Handels error code 404
+    """
+    code_response = {
+            "error": "Not fount"
+    }
+    return jsonify(code_response), 404
 
 if __name__ == "__main__":
     """
