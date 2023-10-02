640dab8bad7d164ebeb38565ea5a02f15247ee95
Amina Amer <amina.amer.9898@gmail.com>
2023-10-02 12:48:24 -0700

    fix style

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,10 +11,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """Handles teardown by calling storage.close()"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
