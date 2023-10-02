0eca7289fe3a3a44b5fbf503cdfec6c9467b486c
Amina Amer <amina.amer.9898@gmail.com>
2023-10-02 13:08:18 -0700

    Make excutable

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,9 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
+    """
+    Main App
+    """
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
