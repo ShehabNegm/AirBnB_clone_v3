bf67033e1fe9054722a00f381bc8b4cc1aafdbb1
Amina Amer <amina.amer.9898@gmail.com>
2023-10-02 17:17:12 -0700

    Add places handler

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
-"""states view"""
+"""places view"""
 
 from flask import jsonify, make_response, request, abort
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
+from models.place import Place
 
 
-@app_views.route('/amenities/', methods=['GET', 'POST'], strict_slashes=False)
-def all_amenities():
-    """function to get all states or post new one"""
+@app_views.route('/places/', methods=['GET', 'POST'], strict_slashes=False)
+def all_places():
+    """function to get all places or post new one"""
     if request.method == 'GET':
-        return jsonify([amenity.to_dict()
-                       for amenity in storage.all('Amenity').values()])
+        return jsonify([place.to_dict()
+                       for place in storage.all('Place').values()])
     if request.method == 'POST':
         if not request.json:
             abort(400, 'Not a JSON')
         if 'name' not in request.json:
             abort(400, 'Missing name')
-        new_amenity = Amenity(**request.get_json())
-        new_amenity.save()
-        return make_response(jsonify(new_amenity.to_dict()), 201)
+        new_place = Place(**request.get_json())
+        new_place.save()
+        return make_response(jsonify(new_place.to_dict()), 201)
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'],
+@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def id_amenity(amenity_id):
-    """get state by its id"""
-    amenity = storage.get('Amenity', amenity_id)
+def id_place(place_id):
+    """get place by its id"""
+    place = storage.get('Place', place_id)
 
-    if not amenity:
+    if not place:
         abort(404)
 
     if request.method == 'GET':
-        return jsonify(amenity.to_dict())
+        return jsonify(place.to_dict())
 
     if request.method == 'DELETE':
         try:
-            storage.delete(amenity)
+            storage.delete(place)
             storage.save()
             return make_response(jsonify({}), 200)
         except Exception:
@@ -47,6 +47,6 @@ def id_amenity(amenity_id):
             abort(400, 'Not a JSON')
         for k, v in request.json.items():
             if k not in ['id', 'created_at', 'updated_at']:
-                setattr(amenity, k, v)
-            amenity.save()
-            return make_response(jsonify(amenity.to_dict()), 200)
+                setattr(place, k, v)
+            place.save()
+            return make_response(jsonify(place.to_dict()), 200)
